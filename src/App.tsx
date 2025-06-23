6c776a074fa3b3bd2c393c49efcfe330010516d9
THE WAVEY STATION <thewaveystation@Waveys-MacBook-Pro.local>
2025-06-23 13:18:14 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Layout from "./components/Layout"
+import AboutUs from "./components/sections/AboutUs"
+import { Navbar } from "./components/elements/Navbar"
+
+
+function App() {
+
+  return (
+
+    <>
+    <Layout title= "CR24">
+    <Navbar/>
+    </Layout>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Layout.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import { Navbar } from "./elements/Navbar"
+// import Foote
+
+interface LayoutProps {
+    title: string;
+    children: React.ReactNode;
+}
+const Layout = ({ title, children }: LayoutProps) => {
+    useEffect(() => {
+        document.title = title;
+    }, [title])
+    return (
+        <>
+            <Navbar />
+            <main className="flex flex-col gap-y-20 md:gap-y-32 overflow-hidden">
+                {children}
+            </main>
+
+        </>
+    )
+}
+
+export default Layout
