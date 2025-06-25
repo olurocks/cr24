9bd8266c351fa9dc9a1dd451a1a68bd6e6019de6
olur0cks <olurocks1@gmail.com>
2025-06-25 12:37:07 +0100

    landing page

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
 const CallToAction = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default CallToAction
+export default CallToAction;
